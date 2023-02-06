127763db63a95368c5dfc999003c4e00f0346559
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-02-06 14:26:06 -0500

    Started bonus

---
+++ Project0/bonus.py
@@ -0,0 +1,106 @@
+# -*-coding:utf-8-*-
+# Copyright (c) 2020 DJI.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from pupil_apriltags import Detector
+import cv2
+import numpy as np
+from robomaster import robot
+from robomaster import camera
+
+at_detector = Detector(
+    families="tag36h11",
+    nthreads=1,
+    quad_decimate=1.0,
+    quad_sigma=0.0,
+    refine_edges=1,
+    decode_sharpening=0.25,
+    debug=0
+)
+
+def find_pose_from_tag(K, detection):
+    m_half_size = tag_size / 2
+
+    marker_center = np.array((0, 0, 0))
+    marker_points = []
+    marker_points.append(marker_center + (-m_half_size, m_half_size, 0))
+    marker_points.append(marker_center + ( m_half_size, m_half_size, 0))
+    marker_points.append(marker_center + ( m_half_size, -m_half_size, 0))
+    marker_points.append(marker_center + (-m_half_size, -m_half_size, 0))
+    _marker_points = np.array(marker_points)
+
+    object_points = _marker_points
+    image_points = detection.corners
+
+    pnp_ret = cv2.solvePnP(object_points, image_points, K, distCoeffs=None,flags=cv2.SOLVEPNP_IPPE_SQUARE)
+    if pnp_ret[0] == False:
+        raise Exception('Error solving PnP')
+
+    r = pnp_ret[1]
+    p = pnp_ret[2]
+
+    return p.reshape((3,)), r.reshape((3,))
+
+if __name__ == '__main__':
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+    ep_camera = ep_robot.camera
+    ep_camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+    ep_chassis = ep_robot.chassis
+
+    tag_size=0.16 # tag size in meters
+
+    target = np.array([0, 0.5]) # 0.5 m away from tag
+
+    while True:
+        try:
+            img = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)   
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray.astype(np.uint8)
+
+            K=np.array([[184.752, 0, 320], [0, 184.752, 180], [0, 0, 1]])
+
+            results = at_detector.detect(gray, estimate_tag_pose=False)
+
+            for res in results:
+                # If AprilTag in frame, only 1 result 
+                pose = find_pose_from_tag(K, res)
+                rot, jaco = cv2.Rodrigues(pose[1], pose[1])
+
+                pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
+                img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
+                cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
+
+                t = np.array([pose[0][0], pose[0][2]])
+
+                error = t - target
+                dir_ = error / np.linalg.norm(error)
+                out = error * 0.5 # Tag moves at 0.5 m/s
+                out = out * 60 * 100 / np.pi / 10
+
+                x_speed = out[1]
+                y_speed = 0
+                z_speed = out[0]
+                print(x_speed, y_speed, z_speed)
+                ep_chassis.drive_speed(x=x_speed, y=y_speed, z=z_speed, timeout=0.1)
+
+            cv2.imshow("img", img)
+            cv2.waitKey(10)
+
+        except KeyboardInterrupt:
+            ep_camera.stop_video_stream()
+            ep_robot.close()
+            print('Exiting')
+            exit(1)

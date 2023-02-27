72ab203b713b981f63f8ab586305c37cfb9d2705
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-02-27 14:26:02 -0500

    Heading control

---
+++ Project1/heading_control.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def heading_control(p_ad_c, p_a_c):
+    """
+    Params:
+        p_ad_c: Desired april tag direction in camera frame coordinates
+        P_a_c:  Actual april tag direction in camera frame coordinates
+        Both are in R2 because y-coords should be 0
+    Returns:
+        Velocity that the robot should rotate at
+    """
+    K = 1
+    return -K * np.cross(p_ad_c, p_a_c)[0]
+    # Cross product is in R1 because p_ad_c and p_a_c are in R2

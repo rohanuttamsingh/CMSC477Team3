aec1dea8e09253bc8d3d0950781720bf112d6168
rightfootmessi <aroni.g1011@gmail.com>
2023-03-01 16:04:04 -0500

    get world coords from tag 32

--- Project1/main.py
+++ Project1/main.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from robomaster import robot
 from robomaster import camera
+import tagmap
 
 at_detector = Detector(
     families="tag36h11",
@@ -39,12 +40,45 @@ def find_pose_from_tag(K, detection):
 
     return p.reshape((3,)), r.reshape((3,))
 
+def to_world_coords(tag_id, posc, rotc):
+    if tag_id in tagmap.tags_top:
+        Rwa = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
+    elif tag_id in tagmap.tags_left:
+        Rwa = np.array([[0, 0, -1], [-1, 0, 0], [0, 1, 0]])
+    elif tag_id in tagmap.tags_right:
+        Rwa = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+    else:     # in tagmap.tags_bottom
+        Rwa = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
+
+    tag_pos = tagmap.tagmap[tag_id]
+    twa = np.array([[tag_pos[0]], [tag_pos[1]], 0])
+
+    Rca = rotc
+    tca = posc[0]
+    pa = np.matrix.transpose([0, 0, 0, 1])
+	
+    Tca = np.zeros((4,4))
+    Tca[0:3,0:3] = Rca
+    Tca[0:3,3] = tca
+    Tca[3,3] = 1
+	
+    Tac = np.linalg.inv(Tca)
+
+    Twa = np.zeros((4,4))
+    Twa[0:3,0:3] = Rwa
+    Twa[0:3,3] = twa
+    Twa[3,3] = 1
+
+    return Twa@Tac@pa
+    
+
 if __name__ == "__main__":
     """
     Program flow:
         startup -> assume roughly on starting point, facing towards 32
         detect 32 and figure out exact location
         calculate path
+        move along path?
         ...
     """
     ep_robot = robot.Robot()
@@ -57,7 +91,13 @@ if __name__ == "__main__":
 
     target = np.array([0, 0.5]) # 0.5 m away from tag
 
-    while True:
+    found_32 = False
+    pose = None
+    rot = None
+
+    # NOT the main control loop - at first we ONLY want to find tag 32
+    # this is to determine our precise starting location
+    while not found_32:
         try:
             img = ep_camera.read_cv2_image(strategy="newest", timeout=0.5)   
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -69,15 +109,19 @@ if __name__ == "__main__":
 
             for res in results:
                 # If AprilTag in frame, only 1 result 
-                pose = find_pose_from_tag(K, res)
-                rot, jaco = cv2.Rodrigues(pose[1], pose[1])
+                _pose = find_pose_from_tag(K, res)
+                _rot, jaco = cv2.Rodrigues(_pose[1], _pose[1])
 
                 pts = res.corners.reshape((-1, 1, 2)).astype(np.int32)
                 img = cv2.polylines(img, [pts], isClosed=True, color=(0, 0, 255), thickness=5)
                 cv2.circle(img, tuple(res.center.astype(np.int32)), 5, (0, 0, 255), -1)
                 id = res.tag_id#.decode("utf-8")
-                print(id)
-                t = np.array([pose[0][0], pose[0][2]])
+                if id == 32:
+                    found_32 = True
+                    pose = _pose
+                    rot = _rot
+            if found_32:
+                break
 
             cv2.imshow("img", img)
             cv2.waitKey(10)
@@ -88,3 +132,6 @@ if __name__ == "__main__":
             print('Exiting')
             exit(1)
 
+    current_pos = to_world_coords(32, pose, rot)
+
+    

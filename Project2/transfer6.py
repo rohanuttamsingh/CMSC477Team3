a7669c4b4d8e3209bc635a8cd59d13c275b0b5ea
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-03-13 16:53:25 -0400

    Minor

--- Project2/transfer6.py
+++ Project2/transfer6.py
@@ -7,22 +7,22 @@ def transfer6(xd,yd):
 
     ep_gripper = ep_robot.gripper
     ep_arm = ep_robot.robotic_arm
+    ep_arm.recenter()
     # open gripper
     ep_gripper.open(power=50)
     time.sleep(3)
     ep_gripper.pause()
     # Move to xd,yd
-    ep_arm.move(x=xd, y=0).wait_for_completed()
-    ep_arm.move(x=0, y=yd).wait_for_completed()
+    ep_arm.moveto(x=xd, y=yd).wait_for_completed()
     # close gripper
     ep_gripper.close(power=50)
     time.sleep(3)
     ep_gripper.pause()
     # Move to original position
-    ep_arm.move(x=0, y=-yd).wait_for_completed()
+    ep_arm.moveto(x=0, y=0).wait_for_completed()
     ep_arm.move(x=-xd, y=0).wait_for_completed()
 
     ep_robot.close()
 
 if __name__ == '__main__':
-    transfer6(100, -100)
+    transfer6(170, -80)

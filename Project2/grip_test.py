619763dc97eb0c5bfc51497c7828b14137757e2e
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-03-13 16:50:44 -0400

    Moveto

--- Project2/grip_test.py
+++ Project2/grip_test.py
@@ -14,21 +14,14 @@ if __name__ == '__main__':
     time.sleep(1)
     ep_gripper.pause()
 
-    # Move forward 20mm
-    ep_arm.move(x=50, y=0).wait_for_completed()
-
-    # # Move downward 20mm
-    ep_arm.move(x=0, y=-75).wait_for_completed()
+    ep_arm.moveto(x=200, y=-50).wait_for_completed()
 
     # close gripper
     ep_gripper.close(power=50)
     time.sleep(1)
     ep_gripper.pause()
 
-    # Move upward 20mm
-    ep_arm.move(x=0, y=75).wait_for_completed()
-
     # Move backward 20mm
-    ep_arm.move(x=-50, y=0).wait_for_completed()
+    ep_arm.move(x=0, y=0).wait_for_completed()
 
     ep_robot.close()

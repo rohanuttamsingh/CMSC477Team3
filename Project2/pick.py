1a368656b798f579c7c38f8766c9e5ba65620e1f
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-03-27 15:16:31 -0400

    Pick

--- Project2/pick.py
+++ Project2/pick.py
@@ -1,7 +1,11 @@
 import time
 import robomaster
 from robomaster import robot
+
 def pick():
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+
     ep_gripper = ep_robot.gripper
     ep_arm = ep_robot.robotic_arm
     # open gripper
@@ -9,10 +13,14 @@ def pick():
     time.sleep(1)
     ep_gripper.pause()
     # Move to 150,-40 which is where the block will be 
-    ep_arm.move(x=150, y=-40).wait_for_completed()
+    ep_arm.move(x=150, y=-50).wait_for_completed()
     # close gripper
     ep_gripper.close(power=50)
-    time.sleep(10)
+    time.sleep(1)
     ep_gripper.pause()
     # Move to original position
-    ep_arm.move(x=-150, y=-40).wait_for_completed()
+    ep_arm.move(x=-130, y=50).wait_for_completed()
+    ep_robot.close()
+
+if __name__ == '__main__':
+    pick()

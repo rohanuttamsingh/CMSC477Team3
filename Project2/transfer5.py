67dce60d2756136c7970359bb48462d33d61d178
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2023-03-27 14:52:07 -0400

    transfer6 is actually pickup

--- Project2/transfer5.py
+++ Project2/transfer5.py
@@ -1,17 +1,16 @@
 import time
 import robomaster
 from robomaster import robot
+
 def transfer5():
+    ep_robot = robot.Robot()
+    ep_robot.initialize(conn_type="ap")
+
     ep_gripper = ep_robot.gripper
     ep_arm = ep_robot.robotic_arm
-    #Sleep for 5 seconds once transfer5 is called 
+    # Sleep for 5 seconds once transfer5 is called 
     time.sleep(10)
     # open gripper
     ep_gripper.open(power=50)
     time.sleep(5)
     ep_gripper.pause()
-
-
- 
-
-   
